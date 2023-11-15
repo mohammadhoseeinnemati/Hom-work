5178c4c74c78878b4a6928737708694f05a91298
mohammad <mohammadhoseeinnemati1384@gmail.com>
2023-11-15 07:33:59 -0800

    first commit

---
+++ Hom.py
@@ -0,0 +1,68 @@
+#Exam python
+
+name = input("plz enter your name? ")
+family = input("plz enter your family name? ")
+seat = input("plz enter Seat number? ")
+
+input("plz enter your phone namber or gmail: ")
+
+pasword_1 = input("pliz create your pasword: ")
+pasword_2 = input("pliz enter your pasword: ")
+
+for pasword in range(2):
+  while  pasword_1 != pasword_2:
+    print("rong pasword")
+    pasword_2 = input("try again")
+print("your pasword is accepted")
+###########*********************###################
+print("What is a correct syntax to output 'Hello World' in Python?")
+
+greens_list = {"4 echo'hello world' " , "1 p ('hello world') " , "2 print ('hello world') " , "3 echo ('hello world')" }
+
+for greens in greens_list:
+  print(greens)
+option_1 = input("option_1: ")
+if option_1 == "2":
+  print("you win")
+elif option_1 != "2":
+  print("it is wrong")
+else:
+  print("What is the correct file extension for Python files?")
+  
+greens_list2 = {"1 pt","2 pyt","3 pyth","4 py" }
+for greens in greens_list2:
+  print(greens)
+question_2 = input("question_2:") 
+if question_2 == "4":
+  print("you win")
+elif question_2 != "4":
+  print("it is wrong")
+else:
+  print("How do you create a variable with the floating number 2.8?")
+
+greens_list3 = {"1 x=2.8","2 both the other answers are correct","3 x = float(2.8)"}
+for greens in greens_list3:
+  print(greens)
+question_3 = input("question_3:")
+if question_3 == "3":
+  print("you win")
+elif question_3 != "4":
+  print("it is wrong")
+else:
+  print("Which operator is used to multiply numbers?")
+greens_list4 = {"1 *" ,"2 /","3 +","4 -"}
+for greens in greens_list4:
+  print(greens)
+question_4 = input("question_4:")
+if question_4 == "1":
+  print("you win")
+elif question_4 != "1":
+  print("it is wrong")
+else:
+  print("good luck")
+
+
+
+
+
+
